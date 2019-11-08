0bbd0a3b351dfe37eb990f32854dc44b943a9b3f
Ben <bsmiller25@gmail.com>
2019-11-08 17:31:18 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/pets/care')
+def petcare():
+    return render_template('petcare.html')
